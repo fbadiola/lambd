98b6bf241f8611826028e83ba08b1d7c1f12ee3a
Federico Badiola <federico.badiola@esic.edu>
2017-10-26 16:10:10 +0200

    change version and fix request.body

--- src/Request.js
+++ src/Request.js
@@ -7,7 +7,7 @@ class Request {
   parseBody() {
     let body;
     try {
-      body = JSON.parse(this.event.body || {});
+      body = JSON.parse(this.event.body || '{}');
     } catch (e) {
       body = this.event.body;
     }
