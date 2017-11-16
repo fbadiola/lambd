fa24a4ebb1c081b19da6d458ce1c14cb0bb9d838
Federico Badiola <federico.badiola@esic.edu>
2017-11-16 11:38:36 +0100

    methodHOF: check if promiseFn returns a promise

--- src/hof/methodHOF.js
+++ src/hof/methodHOF.js
@@ -1,7 +1,25 @@
+const isPromise = (obj) => !!(obj && obj.then && obj.then.call && obj.catch && obj.catch.call);
+const isFunction = (obj) => !!obj.call;
+
 const methodHOF = (method, promiseFn) => (fn) => (options) => {
   const { request, response } = options;
   if (request.method.toLowerCase() === method.toLowerCase()) {
-    promiseFn(options).then(result => response.json(result));
+    if (isFunction(promiseFn)) {
+      const promise = promiseFn(options);
+      if (isPromise(promise)) {
+        promise
+          .then(result => response.json(result))
+          .catch(error => {
+            let message = error;
+            let code = 500;
+            if (error) {
+              code = error.code || code;
+              message = error.message || message;
+            }
+            return response.status(code).error(message);
+          });
+      }
+    }
   } else {
     fn(options);
   }
